af940135c121f5ae5f3ff807f66354575f748ca7
Fumino <274212303@qq.com>
2023-12-26 02:49:39 +0800

    add:new data about ed

--- test.py
+++ test.py
@@ -2,38 +2,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import os
-
 import sklearn.metrics
+import warnings
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras.layers import Dense,LSTM
+from keras.losses import CategoricalCrossentropy
 
 print(os.listdir())
 
-import warnings
-
 warnings.filterwarnings('ignore')
 
-dataset_alq = pd.read_csv('ALQ.csv')
-dataset_trigly = pd.read_csv('TRIGLY.csv')
-
-# delete rows including na
-dataset_alq = dataset_alq.dropna()
-dataset_trigly = dataset_trigly.dropna()
+dataset_ed = pd.read_csv('ED.csv')
+dataset_b12 = pd.read_csv('B12.csv')
 
 # merge datasets and remove SQEN col, reset index, drop unused cols
-merged_dataset = pd.merge(dataset_trigly, dataset_alq, on='SEQN', how='left').dropna()
-merged_dataset = merged_dataset.drop(columns=['ALQ111','ALQ151', 'SEQN', 'LBDTRSI', 'LBDLDLSI',
-                                              'LBDLDL', 'LBDLDLM', 'LBDLDMSI', 'LBDLDLN',
-                                              'LBDLDNSI', 'WTSAFPRP'])
+merged_dataset = pd.merge(dataset_ed, dataset_b12, on='SEQN', how='left')
+print(merged_dataset)
+merged_dataset = merged_dataset.loc[:, ['KIQ400', 'LBDFOLSI', 'LBDB12SI']]
 
+# delete rows including na
+merged_dataset = merged_dataset.dropna()
+print(merged_dataset)
 # based docs, drop ALQ values:77 99 777 999 ,'ALQ121' 'ALQ130','ALQ142',
 #                                                 'ALQ270','ALQ280','ALQ290',
 #                                                 'ALQ151','ALQ170'
 
-for col in ['ALQ121', 'ALQ130', 'ALQ142', 'ALQ270', 'ALQ280', 'ALQ290', 'ALQ170']:
-    drop_index = merged_dataset[merged_dataset[col].isin([77, 99, 777, 999])].index
+for col in ['KIQ400']:
+    drop_index = merged_dataset[merged_dataset[col].isin([7, 9])].index
     print(drop_index, 'deleted.')
     merged_dataset.drop(drop_index, inplace=True)
 
@@ -41,49 +40,68 @@ merged_dataset = merged_dataset.reset_index(drop=True)
 
 print(merged_dataset)
 
+# LBDFOLSI = merged_dataset['LBDFOLSI']
+# merged_dataset['LBDFOLSI'] = LBDFOLSI.map(lambda x: 0 if x < 6.8 else 1)
+# LBDB12SI = merged_dataset['LBDB12SI']
+# merged_dataset['LBDB12SI'] = LBDB12SI.map(lambda x: 0 if x < 156 else 1)
+
+print(merged_dataset)
+
 # analysing the target var
-LBXTR = merged_dataset['LBXTR']
+KIQ400 = merged_dataset['KIQ400']
+# merged_dataset['KIQ400'] = KIQ400
 
 # check correlation between columns
-print(merged_dataset.corr()['LBXTR'].abs().sort_values(ascending=False))
+print(merged_dataset.corr()['KIQ400'].abs().sort_values(ascending=False))
 
-
-def category_lbxtr(x):
-    if x < 100:
-        return 1
-    elif x < 150:
-        return 2
-    else:
-        return 3
-
-
-LBXTR_preprocessed = LBXTR.map(category_lbxtr)
-merged_dataset['LBXTR'] = LBXTR_preprocessed
-value_counts = LBXTR_preprocessed.value_counts().sort_index()
+value_counts = KIQ400.value_counts().sort_index()
 
 sns.barplot(x=value_counts.index, y=value_counts.values, orient='h')
 
-plt.xticks([0, 1, 2, 3])
-plt.xlabel('LBXTR level')
+plt.xticks([0, 1, 2, 3, 4])
+plt.xlabel('KIQ400 level')
 plt.ylabel('Count')
-plt.title('LBXTR level Count')
+plt.title('KIQ400 level Count')
 
+plt.show()
+
+# get counts
+# LBDB12SI = merged_dataset['LBDB12SI']
+# value_counts = LBDB12SI.value_counts().sort_index()
+#
+# sns.barplot(x=value_counts.index, y=value_counts.values, orient='h')
+#
+# plt.xticks([0, 1])
+# plt.xlabel('LBDB12SI level')
+# plt.ylabel('Count')
+# plt.title('LBDB12SI level Count')
+# plt.show()
+#
+# LBDFOLSI = merged_dataset['LBDFOLSI']
+# value_counts = LBDFOLSI.value_counts().sort_index()
+#
+# sns.barplot(x=value_counts.index, y=value_counts.values, orient='h')
+#
+# plt.xticks([0, 1])
+# plt.xlabel('LBDFOLSI level')
+# plt.ylabel('Count')
+# plt.title('LBDFOLSI level Count')
 # plt.show()
 
-predictors = merged_dataset.drop("LBXTR", axis=1)
-target = merged_dataset["LBXTR"]
+predictors = merged_dataset.drop("KIQ400", axis=1)
+# normalization
+# predictors = (predictors - predictors.min()) / (predictors.max()-predictors.min())
+# print(predictors)
+target = merged_dataset["KIQ400"]
 
 X_train, X_test, Y_train, Y_test = train_test_split(predictors, target, test_size=0.20, random_state=0)
 print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
 
-from keras.models import Sequential
-from keras.layers import Dense
-
 model = Sequential()
-model.add(Dense(12, activation='relu', input_dim=7))
-model.add(Dense(1, activation='sigmoid'))
+model.add(LSTM(128,activation='relu',input_shape=(2,1)))
+model.add(Dense(1))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
 model.fit(X_train, Y_train, epochs=300)
 
